140f4225bee4355ea415b2e8adda494a49711f60
yul <yu.li@tecsys.com>
2020-05-30 07:48:33 -0400

    Initial commit

---
+++ src/main/java/com/yulikexuan/jsonlab/JsonlabApplication.java
@@ -0,0 +1,13 @@
+package com.yulikexuan.jsonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonlabApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yulikexuan/jsonlab/model/BeerDto.java
@@ -0,0 +1,41 @@
+//: com.yulikexuan.jsonlab.model.BeerDto.java
+
+
+package com.yulikexuan.jsonlab.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@Builder @AllArgsConstructor
+public class BeerDto {
+
+    @Null
+    private UUID id;
+
+    @NotBlank
+    private String beerName;
+
+    @NotBlank
+    private String beerStyle;
+
+    @Positive
+    private Long upc;
+
+    private BigDecimal price;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
+
+}///:~
